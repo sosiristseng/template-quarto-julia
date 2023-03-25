24f387cdf64b4494a03cb848e7206f880ee874d2
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-03-25 16:46:04 +0800

    multiprocessing

--- literate.jl
+++ literate.jl
@@ -1,5 +1,24 @@
-using Literate
+using Distributed
 
-cd("docs")
+# For all processes
+@everywhere begin
+    import Pkg
+    Pkg.activate(@__DIR__)
 
-Literate.notebook("plots.jl")
+    using Literate
+    config = Dict("mdstrings" => true)
+end
+
+folder = joinpath(@__DIR__, "docs")
+
+nbs = (
+    "plots.jl",
+)
+
+ts = pmap(nbs; on_error=identity) do nb
+    @elapsed Literate.notebook(joinpath(folder, nb), folder; config)
+end
+
+for (nb, t) in zip(nbs, ts)
+    println(nb, " elapsed/error: ", t)
+end
