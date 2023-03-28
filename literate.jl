2ff0dcacfe08cacc0fae739deed77a94aa3f3598
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-03-28 14:12:05 +0800

    CI

--- literate.jl
+++ literate.jl
@@ -1,24 +1,22 @@
 using Distributed
+using Literate
+using PrettyTables
 
 # For all processes
 @everywhere begin
+    ENV["GKSwstype"] = 100
     import Pkg
     Pkg.activate(@__DIR__)
-
-    using Literate
-    config = Dict("mdstrings" => true)
 end
 
+config = Dict("mdstrings" => true)
 folder = joinpath(@__DIR__, "docs")
+nbs = [nb for nb in readdir(folder) if splitext(nb)[end] == ".jl"]
 
-nbs = (
-    "plots.jl",
-)
-
-ts = pmap(nbs; on_error=identity) do nb
+ts = pmap(nbs; on_error=ex->NaN) do nb
     @elapsed Literate.notebook(joinpath(folder, nb), folder; config)
 end
 
-for (nb, t) in zip(nbs, ts)
-    println(nb, " elapsed/error: ", t)
-end
+pretty_table([nbs ts], header=["Notebook", "Elapsed (s)"])
+
+any(isnan, ts) && error("Error(s) occured!")
