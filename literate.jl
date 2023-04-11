00167ef7f04dc2ca89f8169ec8443cf681544095
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-04-11 16:30:41 +0800

    Revert "CI"
    This reverts commit fac6fb59493eb8cc84749871a7769051a2b97bb4.

--- literate.jl
+++ literate.jl
@@ -5,32 +5,23 @@ using PrettyTables
     using Literate
 end
 
-basedir = "docs/"
+folder = joinpath(@__DIR__, "docs")
+nbs = [nb for nb in readdir(folder) if endswith(nb, ".jl")]
 config = Dict("mdstrings" => true, "execute" => true)
 
-nbs = String[]
-
-for (root, dirs, files) in walkdir(basedir)
-    for file in files
-        if (endswith(file, ".jl"))
-            push!(nbs, joinpath(root, file))
-        end
-    end
-end
-
 ts = pmap(nbs; on_error=ex->NaN) do nb
-    @elapsed Literate.notebook(nb, dirname(nb); config)
+    @elapsed Literate.notebook(joinpath(folder, nb), folder; config)
 end
 
 pretty_table([nbs ts], header=["Notebook", "Elapsed (s)"])
 
-# Debug notebooks one by one if there are errors
+# Retry and locate errors
 for (nb, t) in zip(nbs, ts)
     if isnan(t)
         println("Debugging notebook: ", nb)
         try
             withenv("JULIA_DEBUG" => "Literate") do
-                Literate.notebook(nb, tempdir(); config)
+                Literate.notebook(joinpath(folder, nb), folder; config)
             end
         catch e
             println("Error occured in the cell above")
