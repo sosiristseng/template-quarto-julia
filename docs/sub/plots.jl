36bbf4d1a7e27bfeec852959c21a2dbd43184ec5
Wen Wei Tseng <sosiristseng@gmail.com>
2023-05-18 22:02:05 +0800

    CI

---
+++ docs/sub/plots.jl
@@ -0,0 +1,24 @@
+#===
+# Plotting (subfolder)
+
+Using [PyPlot.jl](https://github.com/JuliaPy/PyPlot.jl) since this package is tricky to install and load.
+===#
+
+import PyPlot as plt
+using Random
+Random.seed!(2022)
+
+#---
+
+plt.plot(1:5, rand(1:6, 5))
+plt.gcf()
+
+# ## Runtime information
+
+import Pkg
+Pkg.status()
+
+#---
+
+import InteractiveUtils
+InteractiveUtils.versioninfo()
